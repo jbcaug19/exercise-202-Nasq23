8ff215d4460536d5a660de9fa1e87837e6a94e4d
Nasq23 <honey.menu23@gmail.com>
2019-08-26 16:47:05 -0400

    first javabean

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+public class Student {
+    private long id;
+    private String name;
+    private String age;
+    private String height;
+    private String phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
